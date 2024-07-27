b9e48d6daf657455dbb93eb3697fccac6ea43b79
Vladimir Guerrero <104907260+vladasblood@users.noreply.github.com>
2024-07-27 22:16:01 +0800

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -1,1 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
+/*
+Challenge
+Your task is to create a ERC20 token and deploy it on the Avalanche network for Degen Gaming. The smart contract should have the following functionality:
+
+1. Minting new tokens: The platform should be able to create new tokens and distribute them to players as rewards. Only the owner can mint tokens.
+2. Transferring tokens: Players should be able to transfer their tokens to others.
+3. Redeeming tokens: Players should be able to redeem their tokens for items in the in-game store.
+4. Checking token balance: Players should be able to check their token balance at any time.
+5. Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.
+*/
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Ownable {
+    
+    uint256 public constant GUNPOWDER = 100;
+
+    mapping(address => uint256) public bulletsOwned;
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        _mint(msg.sender, 10 * (10 ** uint256(decimals())));
+    }
+
+    function redeemBullets(uint256 quantity) public {
+        uint256 cost = GUNPOWDER * quantity;
+        require(balanceOf(msg.sender) >= cost, "Insufficient Tokens for Bullets");
+
+        bulletsOwned[msg.sender] += quantity;
+        _burn(msg.sender, cost);
+    }
+
+    function verifyBullets(address user) public view returns (uint256) {
+        return bulletsOwned[user];
+    }
+
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function checkBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+    }
+
+    function transferTokens(address to, uint256 amount) public {
+        require(to != address(0), "Invalid address");
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to transfer");
+        _transfer(msg.sender, to, amount);
+    }
+}
